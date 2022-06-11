1dd094622f66a9328212ac5ce3d944c3c973d93a
minerKtracer <104286945+minerKtracer@users.noreply.github.com>
2022-06-11 09:41:04 +0800

    Labels

--- MY CONTACT TRACING/MY CONTACT TRACING/Form1.Designer.cs
+++ MY CONTACT TRACING/MY CONTACT TRACING/Form1.Designer.cs
@@ -34,6 +34,11 @@
             this.ATextBox3 = new System.Windows.Forms.TextBox();
             this.PNTextBox4 = new System.Windows.Forms.TextBox();
             this.ADDTextBox5 = new System.Windows.Forms.TextBox();
+            this.FNLabel = new System.Windows.Forms.Label();
+            this.SNLabel = new System.Windows.Forms.Label();
+            this.ALabel = new System.Windows.Forms.Label();
+            this.PNLabel = new System.Windows.Forms.Label();
+            this.ADDLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // EButton
@@ -49,7 +54,7 @@
             // FNTextBox1
             // 
             this.FNTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FNTextBox1.Location = new System.Drawing.Point(157, 49);
+            this.FNTextBox1.Location = new System.Drawing.Point(146, 49);
             this.FNTextBox1.Name = "FNTextBox1";
             this.FNTextBox1.Size = new System.Drawing.Size(129, 21);
             this.FNTextBox1.TabIndex = 1;
@@ -57,7 +62,7 @@
             // SNTextBox2
             // 
             this.SNTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SNTextBox2.Location = new System.Drawing.Point(157, 76);
+            this.SNTextBox2.Location = new System.Drawing.Point(146, 76);
             this.SNTextBox2.Name = "SNTextBox2";
             this.SNTextBox2.Size = new System.Drawing.Size(129, 21);
             this.SNTextBox2.TabIndex = 2;
@@ -65,7 +70,7 @@
             // ATextBox3
             // 
             this.ATextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ATextBox3.Location = new System.Drawing.Point(157, 103);
+            this.ATextBox3.Location = new System.Drawing.Point(146, 103);
             this.ATextBox3.Name = "ATextBox3";
             this.ATextBox3.Size = new System.Drawing.Size(129, 21);
             this.ATextBox3.TabIndex = 3;
@@ -73,7 +78,7 @@
             // PNTextBox4
             // 
             this.PNTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PNTextBox4.Location = new System.Drawing.Point(157, 130);
+            this.PNTextBox4.Location = new System.Drawing.Point(146, 130);
             this.PNTextBox4.Name = "PNTextBox4";
             this.PNTextBox4.Size = new System.Drawing.Size(129, 21);
             this.PNTextBox4.TabIndex = 4;
@@ -81,16 +86,72 @@
             // ADDTextBox5
             // 
             this.ADDTextBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ADDTextBox5.Location = new System.Drawing.Point(157, 157);
+            this.ADDTextBox5.Location = new System.Drawing.Point(146, 157);
             this.ADDTextBox5.Name = "ADDTextBox5";
             this.ADDTextBox5.Size = new System.Drawing.Size(175, 21);
             this.ADDTextBox5.TabIndex = 5;
+            // 
+            // FNLabel
+            // 
+            this.FNLabel.AutoSize = true;
+            this.FNLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FNLabel.Location = new System.Drawing.Point(55, 52);
+            this.FNLabel.Name = "FNLabel";
+            this.FNLabel.Size = new System.Drawing.Size(85, 15);
+            this.FNLabel.TabIndex = 6;
+            this.FNLabel.Text = "First Name :";
+            // 
+            // SNLabel
+            // 
+            this.SNLabel.AutoSize = true;
+            this.SNLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SNLabel.Location = new System.Drawing.Point(67, 79);
+            this.SNLabel.Name = "SNLabel";
+            this.SNLabel.Size = new System.Drawing.Size(73, 15);
+            this.SNLabel.TabIndex = 7;
+            this.SNLabel.Text = "Surname :";
+            // 
+            // ALabel
+            // 
+            this.ALabel.AutoSize = true;
+            this.ALabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ALabel.Location = new System.Drawing.Point(101, 106);
+            this.ALabel.Name = "ALabel";
+            this.ALabel.Size = new System.Drawing.Size(39, 15);
+            this.ALabel.TabIndex = 8;
+            this.ALabel.Text = "Age :";
+            this.ALabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // PNLabel
+            // 
+            this.PNLabel.AutoSize = true;
+            this.PNLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PNLabel.Location = new System.Drawing.Point(29, 133);
+            this.PNLabel.Name = "PNLabel";
+            this.PNLabel.Size = new System.Drawing.Size(111, 15);
+            this.PNLabel.TabIndex = 9;
+            this.PNLabel.Text = "Phone Number :";
+            // 
+            // ADDLabel
+            // 
+            this.ADDLabel.AutoSize = true;
+            this.ADDLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ADDLabel.Location = new System.Drawing.Point(74, 160);
+            this.ADDLabel.Name = "ADDLabel";
+            this.ADDLabel.Size = new System.Drawing.Size(66, 15);
+            this.ADDLabel.TabIndex = 10;
+            this.ADDLabel.Text = "Address :";
             // 
             // GetPass
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(355, 261);
+            this.Controls.Add(this.ADDLabel);
+            this.Controls.Add(this.PNLabel);
+            this.Controls.Add(this.ALabel);
+            this.Controls.Add(this.SNLabel);
+            this.Controls.Add(this.FNLabel);
             this.Controls.Add(this.ADDTextBox5);
             this.Controls.Add(this.PNTextBox4);
             this.Controls.Add(this.ATextBox3);
@@ -113,6 +174,11 @@
         private System.Windows.Forms.TextBox ATextBox3;
         private System.Windows.Forms.TextBox PNTextBox4;
         private System.Windows.Forms.TextBox ADDTextBox5;
+        private System.Windows.Forms.Label FNLabel;
+        private System.Windows.Forms.Label SNLabel;
+        private System.Windows.Forms.Label ALabel;
+        private System.Windows.Forms.Label PNLabel;
+        private System.Windows.Forms.Label ADDLabel;
     }
 }
 
--- MY CONTACT TRACING/MY CONTACT TRACING/Form1.cs
+++ MY CONTACT TRACING/MY CONTACT TRACING/Form1.cs
@@ -16,5 +16,10 @@ namespace MY_CONTACT_TRACING
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
