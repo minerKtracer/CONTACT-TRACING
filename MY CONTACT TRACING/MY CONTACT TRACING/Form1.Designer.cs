06df39fdf77e2e1d3e4e0cfaaee723d99a00d6c5
minerKtracer <104286945+minerKtracer@users.noreply.github.com>
2022-06-11 09:34:07 +0800

    Textbox

--- MY CONTACT TRACING/MY CONTACT TRACING/Form1.Designer.cs
+++ MY CONTACT TRACING/MY CONTACT TRACING/Form1.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.EButton = new System.Windows.Forms.Button();
+            this.FNTextBox1 = new System.Windows.Forms.TextBox();
+            this.SNTextBox2 = new System.Windows.Forms.TextBox();
+            this.ATextBox3 = new System.Windows.Forms.TextBox();
+            this.PNTextBox4 = new System.Windows.Forms.TextBox();
+            this.ADDTextBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // EButton
@@ -41,22 +46,73 @@
             this.EButton.Text = "ENTER";
             this.EButton.UseVisualStyleBackColor = true;
             // 
+            // FNTextBox1
+            // 
+            this.FNTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FNTextBox1.Location = new System.Drawing.Point(157, 49);
+            this.FNTextBox1.Name = "FNTextBox1";
+            this.FNTextBox1.Size = new System.Drawing.Size(129, 21);
+            this.FNTextBox1.TabIndex = 1;
+            // 
+            // SNTextBox2
+            // 
+            this.SNTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SNTextBox2.Location = new System.Drawing.Point(157, 76);
+            this.SNTextBox2.Name = "SNTextBox2";
+            this.SNTextBox2.Size = new System.Drawing.Size(129, 21);
+            this.SNTextBox2.TabIndex = 2;
+            // 
+            // ATextBox3
+            // 
+            this.ATextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ATextBox3.Location = new System.Drawing.Point(157, 103);
+            this.ATextBox3.Name = "ATextBox3";
+            this.ATextBox3.Size = new System.Drawing.Size(129, 21);
+            this.ATextBox3.TabIndex = 3;
+            // 
+            // PNTextBox4
+            // 
+            this.PNTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PNTextBox4.Location = new System.Drawing.Point(157, 130);
+            this.PNTextBox4.Name = "PNTextBox4";
+            this.PNTextBox4.Size = new System.Drawing.Size(129, 21);
+            this.PNTextBox4.TabIndex = 4;
+            // 
+            // ADDTextBox5
+            // 
+            this.ADDTextBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ADDTextBox5.Location = new System.Drawing.Point(157, 157);
+            this.ADDTextBox5.Name = "ADDTextBox5";
+            this.ADDTextBox5.Size = new System.Drawing.Size(175, 21);
+            this.ADDTextBox5.TabIndex = 5;
+            // 
             // GetPass
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(355, 261);
+            this.Controls.Add(this.ADDTextBox5);
+            this.Controls.Add(this.PNTextBox4);
+            this.Controls.Add(this.ATextBox3);
+            this.Controls.Add(this.SNTextBox2);
+            this.Controls.Add(this.FNTextBox1);
             this.Controls.Add(this.EButton);
             this.MinimizeBox = false;
             this.Name = "GetPass";
             this.Text = "THE CONTACT TRACING";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button EButton;
+        private System.Windows.Forms.TextBox FNTextBox1;
+        private System.Windows.Forms.TextBox SNTextBox2;
+        private System.Windows.Forms.TextBox ATextBox3;
+        private System.Windows.Forms.TextBox PNTextBox4;
+        private System.Windows.Forms.TextBox ADDTextBox5;
     }
 }
 
