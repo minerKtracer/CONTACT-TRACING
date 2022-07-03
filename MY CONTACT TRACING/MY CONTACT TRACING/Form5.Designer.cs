ce184902110b7acddadcc07a13f6fb7eef0b361b
minerKtracer <104286945+minerKtracer@users.noreply.github.com>
2022-07-03 16:46:05 +0800

    QR Generator Names Add

--- MY CONTACT TRACING/MY CONTACT TRACING/Form5.Designer.cs
+++ MY CONTACT TRACING/MY CONTACT TRACING/Form5.Designer.cs
@@ -28,19 +28,332 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PBoxImage = new System.Windows.Forms.PictureBox();
+            this.fnTB = new System.Windows.Forms.TextBox();
+            this.aTB = new System.Windows.Forms.TextBox();
+            this.gTB = new System.Windows.Forms.TextBox();
+            this.bTB = new System.Windows.Forms.TextBox();
+            this.haTB = new System.Windows.Forms.TextBox();
+            this.pnTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.GQr = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cTB = new System.Windows.Forms.TextBox();
+            this.vTB = new System.Windows.Forms.TextBox();
+            this.erTB = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.QRCaption = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.HELPMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.ABOUTMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.COMPANYMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.EXITMenu = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxImage)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // PBoxImage
+            // 
+            this.PBoxImage.Location = new System.Drawing.Point(309, 73);
+            this.PBoxImage.Name = "PBoxImage";
+            this.PBoxImage.Size = new System.Drawing.Size(178, 161);
+            this.PBoxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBoxImage.TabIndex = 0;
+            this.PBoxImage.TabStop = false;
+            // 
+            // fnTB
+            // 
+            this.fnTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fnTB.Location = new System.Drawing.Point(140, 83);
+            this.fnTB.Name = "fnTB";
+            this.fnTB.Size = new System.Drawing.Size(156, 21);
+            this.fnTB.TabIndex = 1;
+            // 
+            // aTB
+            // 
+            this.aTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aTB.Location = new System.Drawing.Point(140, 108);
+            this.aTB.Name = "aTB";
+            this.aTB.Size = new System.Drawing.Size(100, 21);
+            this.aTB.TabIndex = 2;
+            // 
+            // gTB
+            // 
+            this.gTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gTB.Location = new System.Drawing.Point(140, 135);
+            this.gTB.Name = "gTB";
+            this.gTB.Size = new System.Drawing.Size(100, 21);
+            this.gTB.TabIndex = 3;
+            // 
+            // bTB
+            // 
+            this.bTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bTB.Location = new System.Drawing.Point(140, 216);
+            this.bTB.Name = "bTB";
+            this.bTB.Size = new System.Drawing.Size(109, 21);
+            this.bTB.TabIndex = 3;
+            // 
+            // haTB
+            // 
+            this.haTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.haTB.Location = new System.Drawing.Point(140, 189);
+            this.haTB.Name = "haTB";
+            this.haTB.Size = new System.Drawing.Size(156, 21);
+            this.haTB.TabIndex = 4;
+            // 
+            // pnTB
+            // 
+            this.pnTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnTB.Location = new System.Drawing.Point(140, 162);
+            this.pnTB.Name = "pnTB";
+            this.pnTB.Size = new System.Drawing.Size(100, 21);
+            this.pnTB.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Full Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(96, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Age :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Gender (F/M) :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Phone Number :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Are you fully vaccinated? :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(27, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Home Address :";
+            // 
+            // GQr
+            // 
+            this.GQr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GQr.Location = new System.Drawing.Point(341, 246);
+            this.GQr.Name = "GQr";
+            this.GQr.Size = new System.Drawing.Size(117, 40);
+            this.GQr.TabIndex = 12;
+            this.GQr.Text = "Generate QR";
+            this.GQr.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(60, 219);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Barangay :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(97, 246);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 15);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "City :";
+            // 
+            // cTB
+            // 
+            this.cTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cTB.Location = new System.Drawing.Point(140, 243);
+            this.cTB.Name = "cTB";
+            this.cTB.Size = new System.Drawing.Size(109, 21);
+            this.cTB.TabIndex = 15;
+            // 
+            // vTB
+            // 
+            this.vTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vTB.Location = new System.Drawing.Point(196, 279);
+            this.vTB.Name = "vTB";
+            this.vTB.Size = new System.Drawing.Size(100, 21);
+            this.vTB.TabIndex = 16;
+            // 
+            // erTB
+            // 
+            this.erTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.erTB.Location = new System.Drawing.Point(196, 311);
+            this.erTB.Name = "erTB";
+            this.erTB.Size = new System.Drawing.Size(100, 21);
+            this.erTB.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(75, 314);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(115, 15);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Exposure Risk? :";
+            // 
+            // QRCaption
+            // 
+            this.QRCaption.AutoSize = true;
+            this.QRCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QRCaption.Location = new System.Drawing.Point(53, 41);
+            this.QRCaption.Name = "QRCaption";
+            this.QRCaption.Size = new System.Drawing.Size(196, 24);
+            this.QRCaption.TabIndex = 19;
+            this.QRCaption.Text = "QR CODE PROFILE";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.HELPMenu,
+            this.ABOUTMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(515, 24);
+            this.menuStrip1.TabIndex = 20;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // HELPMenu
+            // 
+            this.HELPMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.EXITMenu});
+            this.HELPMenu.Name = "HELPMenu";
+            this.HELPMenu.Size = new System.Drawing.Size(44, 20);
+            this.HELPMenu.Text = "Help";
+            // 
+            // ABOUTMenu
+            // 
+            this.ABOUTMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.COMPANYMenu});
+            this.ABOUTMenu.Name = "ABOUTMenu";
+            this.ABOUTMenu.Size = new System.Drawing.Size(52, 20);
+            this.ABOUTMenu.Text = "About";
+            // 
+            // COMPANYMenu
+            // 
+            this.COMPANYMenu.Name = "COMPANYMenu";
+            this.COMPANYMenu.Size = new System.Drawing.Size(173, 22);
+            this.COMPANYMenu.Text = "Program Town Inc.";
+            // 
+            // EXITMenu
+            // 
+            this.EXITMenu.Name = "EXITMenu";
+            this.EXITMenu.Size = new System.Drawing.Size(152, 22);
+            this.EXITMenu.Text = "Exit";
             // 
             // SignUpPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(515, 352);
+            this.Controls.Add(this.QRCaption);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.erTB);
+            this.Controls.Add(this.vTB);
+            this.Controls.Add(this.cTB);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.GQr);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnTB);
+            this.Controls.Add(this.haTB);
+            this.Controls.Add(this.bTB);
+            this.Controls.Add(this.gTB);
+            this.Controls.Add(this.aTB);
+            this.Controls.Add(this.fnTB);
+            this.Controls.Add(this.PBoxImage);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "SignUpPage";
             this.Text = "Sign Up";
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxImage)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox PBoxImage;
+        private System.Windows.Forms.TextBox fnTB;
+        private System.Windows.Forms.TextBox aTB;
+        private System.Windows.Forms.TextBox gTB;
+        private System.Windows.Forms.TextBox bTB;
+        private System.Windows.Forms.TextBox haTB;
+        private System.Windows.Forms.TextBox pnTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button GQr;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox cTB;
+        private System.Windows.Forms.TextBox vTB;
+        private System.Windows.Forms.TextBox erTB;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label QRCaption;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem HELPMenu;
+        private System.Windows.Forms.ToolStripMenuItem EXITMenu;
+        private System.Windows.Forms.ToolStripMenuItem ABOUTMenu;
+        private System.Windows.Forms.ToolStripMenuItem COMPANYMenu;
     }
 }
